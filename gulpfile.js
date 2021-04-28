fa5c5646f5b070751a1d84b1aa27aac83d3fb553
romain325 <romainolivier42@gmail.com>
2021-04-28 11:23:20 +0200

    Update Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,20 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     qunit = require('gulp-qunit');
 
-gulp.task('lint', function () {
+gulp.task('lint', gulp.series(function () {
     return gulp.src(['./mention/plugin.js', './tests/test_mention.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(jshint.reporter('fail'));
-});
+}));
 
-gulp.task('bower', function () {
+gulp.task('bower', gulp.series(() => {
     return bower();
-});
+}));
 
-gulp.task('test', ['bower'], function () {
+gulp.task('test', gulp.series('bower', function () {
     return gulp.src('./tests/test_runner.html')
         .pipe(qunit({ timeout: 10 }));
-});
+}));
 
-gulp.task('default', ['lint']);
+gulp.task('default', gulp.series('lint'));
